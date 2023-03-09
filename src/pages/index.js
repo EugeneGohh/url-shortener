d77330d7194a8f6bb051167753bd96bdd6a06d54
Eugene Goh <64187129+EugeneGohh@users.noreply.github.com>
2023-03-09 09:12:22 +0800

    feat(navbar): Add navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { AiFillGithub } from "react-icons/ai";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,29 +15,29 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <main className={styles.main}>
+        {/* Navbar */}
         <div className={styles.description}>
-          {/* <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.js</code>
-          </p>
           <div>
             <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              href="https://github.com/EugeneGohh"
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
+              Built By <p className={inter.className}>Eugene Goh</p>
             </a>
-          </div> */}
+          </div>
+
+          <div>
+            <a
+              href="https://github.com/EugeneGohh/url-shortener"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <AiFillGithub style={{ width: "65px", height: "40px" }} />
+            </a>
+          </div>
         </div>
 
         <div className={styles.center}>
