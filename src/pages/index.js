b0c6457fa9a922af0cfbeee373198cac8e662f22
Eugene Goh <64187129+EugeneGohh@users.noreply.github.com>
2023-03-09 22:04:04 +0800

    chore: Code cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import { AiFillGithub } from "react-icons/ai";
 import SearchInput from "../../components/SearchInput";
 
 const inter = Inter({ subsets: ["latin"] });
