88d848b92b1351f06bfc12a09f4dd6eb67fe1d4b
Eugene Goh <64187129+EugeneGohh@users.noreply.github.com>
2023-03-10 13:27:20 +0800

    feat(modal): Add modal to card component

--- components/Card.js
+++ components/Card.js
@@ -1,4 +1,16 @@
+import { useState } from "react";
+
 function Card() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleShowModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <>
       <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
@@ -17,6 +29,7 @@ function Card() {
           <a
             href="#"
             className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            onClick={handleShowModal}
           >
             View Report
             <svg
@@ -33,6 +46,41 @@ function Card() {
               ></path>
             </svg>
           </a>
+
+          {showModal && (
+            <>
+              <div className="fixed inset-0 z-50 flex items-center justify-center">
+                <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+                <div className="relative bg-white w-1/2 max-w-md mx-auto rounded-lg shadow-lg z-10">
+                  <div className="p-4">
+                    <div class="p-6 space-y-6">
+                      <h1>Report on This Shorten URL</h1>
+                      <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                        Number of Clicks
+                      </p>
+                      <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                        Originating Geolocation
+                      </p>
+                      <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                        Timestamp
+                      </p>
+                    </div>
+
+                    <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                      <button
+                        data-modal-hide="defaultModal"
+                        type="button"
+                        class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+                        onClick={handleCloseModal}
+                      >
+                        Close
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </>
