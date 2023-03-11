e5cdbee6a187328dd57f0d2e057f475211d9e5f7
Eugene Goh <64187129+EugeneGohh@users.noreply.github.com>
2023-03-11 22:10:28 +0800

    fix: Set a base url

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  publicRuntimeConfig: {
+    baseURL: process.env.BASE_URL,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
